06fd93901f22f5189cfe80cb52144d7fbe7ae41b
TerraVulccano <ilya.deryushev@gmail.com>
2021-10-07 20:27:30 +0300

    homework version 1

---
+++ src/test/java/SeacrhForJunit5.java
@@ -0,0 +1,24 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+
+public class SeacrhForJunit5 {
+
+
+    @Test
+    void SearchForJunit5() {
+
+        Selenide.open("https://github.com");
+        Selenide.$("[name=q]").setValue("selenide").pressEnter();
+        Selenide.$$("ul.repo-list li").first().$("a").click();
+        Selenide.$("[data-content=Wiki]").click();
+        Selenide.$("#wiki-body").shouldHave(text("Soft assertions")).click();
+        Selenide.$("#wiki-body").shouldHave(text("Using JUnit5 extend test class:"));
+
+
+        Selenide.sleep(5000);
+    }
+
+}
