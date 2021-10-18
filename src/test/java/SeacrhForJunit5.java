5dab0b022f1fe996542b57996752607fdac12eb5
TerraVulccano <ilya.deryushev@gmail.com>
2021-10-18 19:42:23 +0300

    updated

--- src/test/java/SeacrhForJunit5.java
+++ src/test/java/SeacrhForJunit5.java
@@ -1,11 +1,18 @@
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 
 public class SeacrhForJunit5 {
 
+    @BeforeAll
+    static void setup() {
+        Configuration.startMaximized = true;
+    }
 
     @Test
     void SearchForJunit5() {
@@ -14,8 +21,9 @@ public class SeacrhForJunit5 {
         Selenide.$("[name=q]").setValue("selenide").pressEnter();
         Selenide.$$("ul.repo-list li").first().$("a").click();
         Selenide.$("[data-content=Wiki]").click();
-        Selenide.$("#wiki-body").shouldHave(text("Soft assertions")).click();
+        Selenide.$(byText("Soft assertions")).click();
         Selenide.$("#wiki-body").shouldHave(text("Using JUnit5 extend test class:"));
     }
+
 
 }
